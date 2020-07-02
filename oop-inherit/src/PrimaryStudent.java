934bf62d55d9f8884bcc8109020b0d9c9ea6a332
syj <723809534@qq.com>
2020-07-02 19:37:30 +0800

    继承

---
+++ oop-inherit/src/PrimaryStudent.java
@@ -0,0 +1,10 @@
+
+public class PrimaryStudent extends Student {
+
+	int grade;
+	public PrimaryStudent(String name, int age, int score,int grade) {
+		super(name, age, score);
+		this.grade=grade;
+	}
+
+}
