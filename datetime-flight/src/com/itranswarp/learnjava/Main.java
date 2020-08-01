a05a5319769e28d25463b6d09c0e7f452990b70e
stephen shen <723809534@qq.com>
2020-08-01 11:36:05 +0800

    时区转换

---
+++ datetime-flight/src/com/itranswarp/learnjava/Main.java
@@ -0,0 +1,34 @@
+package com.itranswarp.learnjava;
+
+import java.time.*;
+
+/**
+ * Learn Java from https://www.liaoxuefeng.com/
+ * 
+ * @author liaoxuefeng
+ */
+public class Main {
+	public static void main(String[] args) {
+		LocalDateTime departureAtBeijing = LocalDateTime.of(2019, 9, 15, 13, 0, 0);
+		int hours = 13;
+		int minutes = 20;
+		LocalDateTime arrivalAtNewYork = calculateArrivalAtNY(departureAtBeijing, hours, minutes);
+		System.out.println(departureAtBeijing + " -> " + arrivalAtNewYork);
+		// test:
+		if (!LocalDateTime.of(2019, 10, 15, 14, 20, 0)
+				.equals(calculateArrivalAtNY(LocalDateTime.of(2019, 10, 15, 13, 0, 0), 13, 20))) {
+			System.err.println("测试失败!");
+		} else if (!LocalDateTime.of(2019, 11, 15, 13, 20, 0)
+				.equals(calculateArrivalAtNY(LocalDateTime.of(2019, 11, 15, 13, 0, 0), 13, 20))) {
+			System.err.println("测试失败!");
+		}
+	}
+
+//	某航线从北京飞到纽约需要13小时20分钟，请根据北京起飞日期和时间计算到达纽约的当地日期和时间。
+//	提示：ZonedDateTime仍然提供了plusDays()等加减操作。
+	static LocalDateTime calculateArrivalAtNY(LocalDateTime bj, int h, int m) {
+		ZonedDateTime zny = bj.atZone(ZoneId.of("Asia/Shanghai")).withZoneSameInstant(ZoneId.of("America/New_York"));
+		zny = zny.plusHours(13).plusMinutes(20);
+		return zny.toLocalDateTime();
+	}
+}
